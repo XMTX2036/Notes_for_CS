1241b6b9559a4c8bf2f71b067154ed7fe6f6a498
XMTX2036 <754663901@qq.com>
2022-09-02 23:38:37 +0800

    [CS61A] lab06 finished

---
+++ CS61A/Lab/lab06/lab06.py
@@ -0,0 +1,95 @@
+from audioop import add
+from curses import nonl
+
+
+this_file = __file__
+
+
+def make_adder_inc(a):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2)
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    def adder(b):
+        nonlocal a
+        a += 1
+        return a+b-1
+    return adder
+
+def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    >>> from construct_check import check
+    >>> # Do not use lists in your implementation
+    >>> check(this_file, 'make_fib', ['List'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    f1, f2 = 1, 0
+    def fibonacci():
+        nonlocal f1, f2
+        f1, f2 = f2, f1+f2
+        return f1
+    return fibonacci
+
+def insert_items(lst, entry, elem):
+    """
+    >>> test_lst = [1, 5, 8, 5, 2, 3]
+    >>> new_lst = insert_items(test_lst, 5, 7)
+    >>> new_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> large_lst = [1, 4, 8]
+    >>> large_lst2 = insert_items(large_lst, 4, 4)
+    >>> large_lst2
+    [1, 4, 4, 8]
+    >>> large_lst3 = insert_items(large_lst2, 4, 6)
+    >>> large_lst3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_lst3 is large_lst
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    '''
+    The first way to realize the function would change the id of the lst
+    '''
+    # new_lst = []
+    # for x in lst:
+    #     new_lst.append(x)
+    #     if x == entry:
+    #         new_lst.append(elem)
+    # return new_lst
+    idx = 0
+    size = len(lst)
+    while idx < size:
+        if lst[idx] == entry:
+            lst.insert(idx + 1, elem)
+            idx += 1
+            size += 1
+        idx += 1
+    return lst
